43d37feeb4acbd8452685f221a6dc20ac1a79bdb
Bugra Ates <mbugratesdev@gmail.com>
2022-09-11 18:19:17 +0300

    Add first onboarding screen

--- Bankey/Bankey/Onboarding/OnboardingContainerViewController.swift
+++ Bankey/Bankey/Onboarding/OnboardingContainerViewController.swift
@@ -18,20 +18,20 @@ class OnboardingContainerViewController: UIViewController {
     
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         self.pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
-        
+
         let page1 = ViewController1()
         let page2 = ViewController2()
         let page3 = ViewController3()
-        
+
         pages.append(page1)
         pages.append(page2)
         pages.append(page3)
-        
+
         currentVC = pages.first!
-        
+
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -41,6 +41,7 @@ class OnboardingContainerViewController: UIViewController {
         
         view.backgroundColor = .systemPurple
         
+        // Add child view controllers to a parent view controller
         addChild(pageViewController)
         view.addSubview(pageViewController.view)
         pageViewController.didMove(toParent: self)
